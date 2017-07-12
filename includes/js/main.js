83ce9453f0be4029c168d0dbaf87b583b089ee67
fcarswellwnyc <fcarswell@nypublicradio.org>
2017-07-12 15:01:00 -0400

    fix header and update section text

--- includes/js/main.js
+++ includes/js/main.js
@@ -1,3 +1,19 @@
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 397) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 397) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+});
+
 $(function() {
     
 
