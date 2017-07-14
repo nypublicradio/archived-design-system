7e96c1edeecf63cdc425a8363fa8bbbf9f7872b6
fcarswellwnyc <fcarswell@nypublicradio.org>
2017-07-14 16:33:41 -0400

    add modal designs

--- includes/js/main.js
+++ includes/js/main.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
 });
 
 
-
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
